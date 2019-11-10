dd1fa94a38dc7ed3417186d1db25637bfca9a633
MarcJenningsUK <marc.jenning.s@gmail.com>
2019-11-10 12:18:28 +0000

    Add project files.

---
+++ ImageResizer/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageResizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            DirectoryInfo di = new DirectoryInfo(InputFolderTB.Text);
+            var files = di.GetFiles("*.jpg");
+                       
+            int counter = 0;
+
+            foreach (var filename in files)
+            {
+                var input = System.Drawing.Image.FromFile(filename.FullName);
+                counter++;
+                
+                var output = Services.ImageTools.ResizeImage(input, 256, 256);
+                var outfile = System.IO.Path.Combine(OutputFolderTB.Text, "output-" + counter + ".jpg");
+                using (var str = new FileStream(outfile, FileMode.Create))
+                {
+                    output.Save(str, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+            }
+        }
+    }
+}
---
+++ ImageResizer/Services/ImageTools.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace ImageResizer.Services
+{
+    public class ImageTools
+    {
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="madWidth">The width to resize to.</param>
+        /// <param name="maxHeight">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int madWidth, int maxHeight)
+        {
+            Decimal ratio = (Decimal)image.Width / (Decimal)image.Height;
+
+            if(ratio > 1)
+            {
+                maxHeight = (int)(maxHeight / ratio);
+            }
+            if(ratio < 1)
+            {
+                madWidth = (int)(madWidth / ratio);
+            }
+
+            var destRect = new Rectangle(0, 0, madWidth, maxHeight);
+            var destImage = new Bitmap(madWidth, maxHeight);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
